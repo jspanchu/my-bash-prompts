1feccb1cac0f08e35414a0aefb16104433f1795e
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2023-12-10 09:11:03 -0500

    Remove git branch, make working dir bold.

--- .colored.prompt.bash
+++ .colored.prompt.bash
@@ -1,3 +1,3 @@
 # A colored bash prompt
 # ( username ) [hostname:(git branch)`pwd`] $
-export PS1="( \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;9m\]\u\[$(tput sgr0)\] ) [\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;117m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;215m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\[$(tput sgr0)\]\[\033[38;5;42m\]\w\[$(tput sgr0)\]] \\$ \[$(tput sgr0)\]"
+export PS1="\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;9m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;117m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;215m\]\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;42m\]\w\[$(tput sgr0)\]\$ "
